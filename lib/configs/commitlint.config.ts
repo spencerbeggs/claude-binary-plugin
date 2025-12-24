0b6992364eb1341d04fcefd97e938f33be15a3eb
C. Spencer Beggs <spencer@savvyweb.systems>
2025-12-23 23:54:24 -0500

    ci(commitlint): add type-enum rule
    Restricts commit types to a predefined list of conventional types
    including build, chore, ci, docs, feat, fix, perf, refactor,
    release, revert, style, and test for better consistency

--- lib/configs/commitlint.config.ts
+++ lib/configs/commitlint.config.ts
@@ -2,5 +2,10 @@ export default {
 	extends: ["@commitlint/config-conventional"],
 	rules: {
 		"body-max-line-length": [2, "always", 300],
+		"type-enum": [
+			2,
+			"always",
+			["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "release", "revert", "style", "test"],
+		],
 	},
 };
