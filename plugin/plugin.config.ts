56916bc1b60cd4ef874d36a43b03bff8c74f45eb
C. Spencer Beggs <spencer@beggs.codes>
2026-03-27 03:00:15 -0400

    fix: types
    Signed-off-by: C. Spencer Beggs <spencer@beggs.codes>

--- plugin/plugin.config.ts
+++ plugin/plugin.config.ts
@@ -1,12 +1,11 @@
 import { ClaudeBinaryPlugin } from "claude-binary-plugin";
+import { Schema } from "effect";
 
 export default ClaudeBinaryPlugin.create({
 	prefix: "TEST_PLUGIN",
-	setup: () => {
-		return {
-			foo: 1,
-		};
-	},
+	options: Schema.Struct({
+		foo: Schema.Number,
+	}),
 	hooks: {
 		SessionStart: [{ name: "init", pipeline: "./hooks/session-start.ts" }],
 		PreToolUse: [{ name: "observe", pipeline: "./hooks/pre-tool-use.ts" }],
